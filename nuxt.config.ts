97464a4b4127cf85d324b0665cc44f3f82fc5af8
Sergey Nagornykh <serega1905@ukr.net>
2024-03-10 14:42:22 +0200

    create tailwind base design system

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    // https://google-fonts.nuxtjs.org/
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          'League Spartan': '100..900',
+        },
+      },
+    ],
+  ],
   css: ['~/assets/main.scss'],
   postcss: {
     plugins: {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,28 @@ export default {
     './error.vue',
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: '1.5rem',
+      base: '2.4rem',
+      xl: '3.6rem',
+    },
+    colors: {
+      purple: '#7c5dfa',
+      'purple-light': '#9277ff',
+      onyx: '#0c0e16',
+      dark: '#1e2139',
+      denim: '#252945',
+      silver: '#DFE3FA',
+      gray: '#888EB0',
+      stone: '#7E88C3',
+      orange: '#EC5757',
+      'orange-light': '#FF9797',
+      light: '#F8F8FB',
+      metal: '#141625',
+    },
+    fontFamily: {
+      sans: ['League Spartan', 'sans-serif'],
+    },
   },
   plugins: [],
 }

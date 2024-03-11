8fc3306e09374a74e7d5d24a8784b41d7660b4d6
Sergey Nagornykh <serega1905@ukr.net>
2024-03-11 21:15:31 +0200

    default layout, remove tailwindCss from dependency

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col lg:min-h-screen lg:flex-row">
+    <div class="h-auto">
+      <AppHeader />
+    </div>
+    <div
+      class="container mx-auto max-w-full px-6 py-8 md:max-w-[672px] md:px-12 md:py-[60px] lg:max-w-[730px] lg:px-0 lg:py-[76px]">
+      <slot />
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div>Invoices page</div>
+    <UButton color="sky" variant="outline">Text button</UButton>
+  </div>
+</template>
+
+<style scoped></style>
